ed1c4cf0817523d5d7c98bb8cd1a3251157d8c60
OPap-12 <arpit180105@gmail.com>
2025-01-10 23:04:01 +0530

    Update and rename simple-interest.sh to simple.sh

---
+++ simple.sh
@@ -0,0 +1,12 @@
+# Project Title
+
+Brief description of what this project does and who it's for.
+
+## Installation
+
+Instructions for installing the project.
+
+```bash
+git clone https://github.com/your-username/your-repo.git
+cd your-repo
+npm install
